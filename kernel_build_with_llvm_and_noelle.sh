db35fb2c95ed56238d225ac09c5fccc8b84507e3
Souradip Ghosh <souradip@cmu.edu>
2021-11-30 23:07:44 +0000

    artifact: add scripts for NAS and parsec builds, run script, modify kernel build script, add empty ramdisk

--- kernel_build_with_llvm_and_noelle.sh
+++ kernel_build_with_llvm_and_noelle.sh
@@ -7,16 +7,16 @@ NAUT=`pwd`
 
 # Install LLVM 9.0.0
 LLVM_TOP_DIR=${NAUT}/9.0.0
-# git clone https://github.com/sgh185/LLVM_installer.git ${LLVM_TOP_DIR} ;
-# cd ${LLVM_TOP_DIR} && make && cd ${NAUT} ; 
+git clone https://github.com/sgh185/LLVM_installer.git ${LLVM_TOP_DIR} ;
+cd ${LLVM_TOP_DIR} && make && cd ${NAUT} ; 
 
 # Use the newly installed LLVM
 source ${LLVM_TOP_DIR}/enable ;
 
 # Install Noelle
 NOELLE_TOP_DIR=${NAUT}/noelle
-# git clone https://github.com/scampanoni/noelle.git ${NOELLE_TOP_DIR} ;
-# cd ${NOELLE_TOP_DIR} && make && cd ${NAUT} ;
+git clone https://github.com/scampanoni/noelle.git ${NOELLE_TOP_DIR} ;
+cd ${NOELLE_TOP_DIR} && make && cd ${NAUT} ;
 
 # Use the newly installed Noelle
 source ${NOELLE_TOP_DIR}/enable ;
