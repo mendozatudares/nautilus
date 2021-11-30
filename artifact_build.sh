d8e0ea9bb6d6cd71a6529478e27e32cd67eba0a9
Souradip Ghosh <sgi9754@peroni.cs.northwestern.edu>
2021-11-30 15:54:24 -0600

    artifact: fix noelle invocation in nautilus build

--- artifact_build.sh
+++ artifact_build.sh
@@ -7,16 +7,16 @@ NAUT=`pwd`
 
 # Install LLVM 9.0.0
 LLVM_TOP_DIR=${NAUT}/9.0.0
-git clone https://github.com/sgh185/LLVM_installer.git ${LLVM_TOP_DIR} ;
-cd ${LLVM_TOP_DIR} && make && cd ${NAUT} ; 
+# git clone https://github.com/sgh185/LLVM_installer.git ${LLVM_TOP_DIR} ;
+# cd ${LLVM_TOP_DIR} && make && cd ${NAUT} ; 
 
 # Use the newly installed LLVM
 source ${LLVM_TOP_DIR}/enable ;
 
 # Install Noelle
 NOELLE_TOP_DIR=${NAUT}/noelle
-git clone https://github.com/scampanoni/noelle.git ${NOELLE_TOP_DIR} ;
-cd ${NOELLE_TOP_DIR} && make && cd ${NAUT} ;
+# git clone https://github.com/scampanoni/noelle.git ${NOELLE_TOP_DIR} ;
+# cd ${NOELLE_TOP_DIR} && make && cd ${NAUT} ;
 
 # Use the newly installed Noelle
 source ${NOELLE_TOP_DIR}/enable ;
@@ -28,4 +28,4 @@ make clean ; make include/autoconf.h ;
 ./scripts/pass_build.sh carat KARAT.cpp ;
 
 # Kernel build
-make -j ; make bitcode ; make karat ; make final ; make isoimage ;
+make -j ; make bitcode ; make karat_noelle ; make final ; make isoimage ;
